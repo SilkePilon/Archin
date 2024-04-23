5a6dba201ccf82a4997f0c32e6e234ddb88e479a
webdivy <uiuxdiv@gmail.com>
2024-04-23 18:28:03 +0530

    nw

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+git add .const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
